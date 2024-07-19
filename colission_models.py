8f25ace140bb018cf9d8ba7368ac52a3f73bd605
Andreea Toca <a.toca@student.rug.nl>
2024-07-19 15:02:24 +0300

    collision_models

---
+++ colission_models.py
@@ -0,0 +1,26 @@
+from autobahn.twisted.component import Component, run
+from twisted.internet.defer import inlineCallbacks
+
+def collision_models():
+
+
+@inlineCallbacks
+def main(session, details):
+    
+    collision_models()
+    
+    session.leave()
+
+wamp = Component(
+	transports=[{
+		"url": "ws://wamp.robotsindeklas.nl",
+		"serializers": ["json"],
+		"max_retries": 0
+	}],
+	realm="rie.6633566bc887f6d074f02f40",
+)
+
+wamp.on_join(main)
+
+if __name__ == "__main__":
+	run([wamp])
