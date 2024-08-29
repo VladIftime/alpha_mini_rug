9d8f95e5aec837c64426e7d87e6412969070c3d1
Andreea Toca <a.toca@student.rug.nl>
2024-08-29 10:44:18 +0200

    key_words finished

--- key_words_english.py
+++ key_words_english.py
@@ -1,19 +1,136 @@
 from autobahn.twisted.component import Component, run
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.util import sleep
-from collections import deque
 
 # global variables
-raw_input = deque(maxlen=20)
+certainty_check = 0
+answer_found = None
+debug_flag = False
+key_words_list = []
 
-def on_keyword(frame):
-    global sess
-    if (
-        "certainty" in frame["data"]["body"]
-        and frame["data"]["body"]["certainty"] > 0.45
-    ):
-        sess.call("rie.dialogue.say", text="Ja")
 
+@inlineCallbacks
+def key_words(session,
+            question = None, 
+            question_lang = 'en', 
+            key_words = None, 
+            key_words_lang = 'en', 
+            time = 3,
+            certainty = 0, 
+            debug = False):
+    
+    global certainty_check
+    global answer_found
+    global debug_flag
+    global key_words_list
+    
+     # Check if the arguments are of the correct type
+    if not isinstance(question, str):
+        raise TypeError("question is not a string")
+    if not isinstance(key_words, list):
+        raise TypeError("key_words is not a list")
+    # check if the list contains only strings
+    else:
+        for word in key_words:
+            if not isinstance(word, str):
+                raise TypeError("key_words is not a list of strings")
+    if not isinstance(time, int):
+        raise TypeError("time is not an integer")
+    if not isinstance(certainty, int):
+        raise TypeError("certainty is not an integer")
+    if not isinstance(debug, bool):
+        raise TypeError("debug is not a boolean")
+    
+    certainty_check = certainty
+    debug_flag = debug 
+    key_words_list = key_words
+
+    # set the language and ask the question
+    yield session.call("rie.dialogue.say", text=question, lang = question_lang)
+
+    # set the language for the stream
+    yield session.call("rie.dialogue.config.language", lang=key_words_lang)
+    
+    # start the listening stream
+    yield session.subscribe(key_words_listen, "rie.dialogue.stt.stream")
+    yield session.call("rie.dialogue.stt.stream")
+
+    # record answers for the amount of time specified in seconds
+    yield sleep (delay = time)
+    
+    # check if the answer was found
+    if answer_found == None:
+        print("No answer found")
+    else:    
+        print("The keyword found: " + answer_found)
+    
+    # close the stream
+    session.call("rie.dialogue.stt.close")
+    
+    # return the found answer
+    return answer_found
+    
+
+def check_words(frame_text):
+    global answer_found
+    global key_words_list
+    
+    # check if any of the key words matches the words heard
+    for word in frame_text.split():
+        word = word.lower()
+        if word in key_words_list:
+            answer_found = word
+            break
+        
+def key_words_listen(frame):
+    global debug_flag
+    global certainty_check
+    
+    # to get the text we look for 3 conditions:
+    # 1. the text found is not an empty string
+    # 2. the text found is "Final" (all words heard are concatenated in one string)
+    # 3. the user wants to choose words based on the "certainty"
+    if (not frame["data"]["body"]["text"] == "" and
+        frame["data"]["body"]["final"] and
+        "certainty" in frame["data"]["body"] and 
+        frame["data"]["body"]["certainty"] > certainty_check):
+            
+            # check the words in the text found
+            check_words(frame["data"]["body"]["text"])
+            
+            # if the debug flag is true, it prints the all words heard
+            if debug_flag:
+                print("The user input is:")
+                print(frame["data"]["body"]["text"])
+        
+@inlineCallbacks  
+def main2(session, details):
+    # define question 1 together with the keywords and answers
+    question_colors = "What is your favorite color?"
+    keywords_colors = ["red", "blue", "green", "yellow", "pink", "orange", "purple"]
+    
+    # call keywords function, setting the language of both question and answer
+    # to English; records the answer with a certainty higher than 0.1 for 5
+    # seconds; with the debug flag set to true, it prints all the words heard
+    answer = yield key_words(session, 
+                            question=question_colors,
+                            question_lang = 'en', 
+                            key_words=keywords_colors,
+                            key_words_lang = 'en',
+                            time=5,
+                            certainty = 0.1, 
+                            debug=True)
+    
+    if answer != None:
+        string = "My favorite color is also " +  answer
+    else:
+        string = "Didn't understand your answer, try again later"    
+
+    yield session.call("rie.dialogue.say", text = string)
+    
+    session.leave()
+    
+    
 
 @inlineCallbacks
 def key_words_simple(question=None, key_words=None, time=5000, debug=False):
@@ -50,57 +167,6 @@ def key_words_simple(question=None, key_words=None, time=5000, debug=False):
             print("The keyword found: " + answer_found)
     
     return answer_found
-
-
-@inlineCallbacks
-def key_words_simple_stream(question=None, key_words=None, time=5000, debug=False):
-    global sess
-    global raw_input
-
-    # ask question
-    yield sess.call("rie.dialogue.say", text=question)
-    # get user input and parse it
-    # user_input = yield sess.call("rie.dialogue.stt.read", time=time)
-    print("function before sleep")
-    print(raw_input)
-    yield sleep(2)
-    print("function after sleep")
-    print(raw_input)
-    user_input = raw_input
-    user_response = ""
-    answer_found = None
-    print(user_input)
-    
-    if debug:
-        print("The user input is:")
-        for frame in user_input:
-            print(frame)
-
-    for user_response in user_input:
-            if user_response != "":
-                for word in user_response.split():
-                    word = word.lower()
-                    print(word)
-                if word in key_words and answer_found is None:
-                    answer_found = word
-                    break
-    
-    if debug:
-        if answer_found == None:
-            print("No answer found")
-        else:    
-            print("The keyword found: " + answer_found)
-    
-    return answer_found
-
-def keywords_listen(frame):
-    global raw_input
-    # print("hello")
-    # print(frame)
-    raw_input.appendleft(frame["data"]["body"]["text"])
-    # print(raw_input)
-    print("hello")
-    print(frame)
 
 @inlineCallbacks
 def main(session, details):
@@ -222,10 +288,10 @@ wamp = Component(
             "max_retries": 0,
         }
     ],
-    realm="rie.66c6eff2afe50d23b76c0fa0",
+    realm="rie.66cddd6bafe50d23b76c2f24",
 )
 
-wamp.on_join(main)
+wamp.on_join(main2)
 
 if __name__ == "__main__":
     run([wamp])
