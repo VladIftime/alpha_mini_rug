b5029d85aa15fd86cb513a5aa290735013858d16
Vlad Iftime <vlad.iftime@solarracinggroningen.nl>
2024-08-29 11:59:15 +0200

    Keywords

--- alpha-mini-rug/alpha_mini_rug/keywords.py
+++ alpha-mini-rug/alpha_mini_rug/keywords.py
@@ -7,16 +7,17 @@ answer_found = None
 debug_flag = False
 key_words_list = []
 
+
 @inlineCallbacks
 def key_words(
     session,
-    question = None, 
-    question_lang = 'en', 
-    key_words = None, 
-    key_words_lang = 'en', 
-    time = 3,
-    certainty = 0, 
-    debug = False
+    question=None,
+    question_lang="en",
+    key_words=None,
+    key_words_lang="en",
+    time=3,
+    certainty=0,
+    debug=False,
 ):
     """
     This function asks a question and waits for the user to respond with a keyword from the list of keywords.
@@ -40,7 +41,7 @@ def key_words(
     global answer_found
     global debug_flag
     global key_words_list
-    
+
     # check if the arguments are of the correct type
     if not isinstance(question, str):
         raise TypeError("question is not a string")
@@ -59,65 +60,74 @@ def key_words(
         raise TypeError("debug is not a boolean")
 
     certainty_check = certainty
-    debug_flag = debug 
+    debug_flag = debug
     key_words_list = key_words
 
-    # set the language and ask the question
-    yield session.call("rie.dialogue.say", text=question, lang = question_lang)
+    yield session.call("rie.dialogue.say", text=question, lang=question_lang)
 
-    # set the language for the stream
     yield session.call("rie.dialogue.config.language", lang=key_words_lang)
-    
-    # start the listening stream
+
     yield session.subscribe(key_words_listen, "rie.dialogue.stt.stream")
     yield session.call("rie.dialogue.stt.stream")
 
     # record answers for the amount of time specified in seconds
-    yield sleep (delay = time)
-    
-    # check if the answer was found
+    yield sleep(delay=time)
+
     if answer_found == None:
         print("No answer found")
-    else:    
+    else:
         print("The keyword found: " + answer_found)
-    
-    # close the stream
+
     session.call("rie.dialogue.stt.close")
-    
-    # return the found answer
+
     return answer_found
 
 
 def check_words(frame_text):
+    """
+    This function checks if the words in the user response are in the list of keywords.
+
+    Args:
+        frame_text (str): The text found in the user response.
+
+    Returns:
+        None
+    """
     global answer_found
     global key_words_list
-    
-    # check if any of the key words matches the words heard
+
     for word in frame_text.split():
         word = word.lower()
         if word in key_words_list:
             answer_found = word
             break
-        
+
+
 def key_words_listen(frame):
+    """
+    This function listens to the user response and checks if the words are in the list of keywords. It checks for 3 conditions:
+    1. The text found is not an empty string
+    2. The text found is Final (all words heard are concatenated in one string)
+    3. The user wants to choose words based on the certainty
+
+    Args:
+        frame (dictionary): The frame dictionary from the robot's camera stream
+
+    Returns:
+        None
+    """
     global debug_flag
     global certainty_check
-    
-    # to get the text we look for 3 conditions:
-    # 1. the text found is not an empty string
-    # 2. the text found is "Final" (all words heard are concatenated in one string)
-    # 3. the user wants to choose words based on the "certainty"
+
     if (
-        not frame["data"]["body"]["text"] == "" and
-        frame["data"]["body"]["final"] and
-        "certainty" in frame["data"]["body"] and 
-        frame["data"]["body"]["certainty"] > certainty_check
-        ):
-            
-            # check the words in the text found
-            check_words(frame["data"]["body"]["text"])
-            
-            # if the debug flag is true, it prints the all words heard
-            if debug_flag:
-                print("The user input is:")
-                print(frame["data"]["body"]["text"])
+        not frame["data"]["body"]["text"] == ""
+        and frame["data"]["body"]["final"]
+        and "certainty" in frame["data"]["body"]
+        and frame["data"]["body"]["certainty"] > certainty_check
+    ):
+        # check the words in the text found
+        check_words(frame["data"]["body"]["text"])
+
+        if debug_flag:
+            print("The user input is:")
+            print(frame["data"]["body"]["text"])
