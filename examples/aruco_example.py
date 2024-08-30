45dd885fe864ee7ac85ddf07db1cc8bff89f749b
Andreea Toca <a.toca@student.rug.nl>
2024-08-30 15:40:35 +0200

    aruco

--- examples/aruco_example.py
+++ examples/aruco_example.py
@@ -2,24 +2,37 @@ from autobahn.twisted.component import Component, run
 from twisted.internet.defer import inlineCallbacks
 from alpha_mini_rug import aruco_detect_markers
 
+ids_code = None
 
-def aruco(frame):
+@inlineCallbacks
+def aruco(session, frame):
+    global ids_code
     corners, ids = aruco_detect_markers(frame)
     print("corners:", corners)
-    print("ids:", ids)
-    pass
+    if not ids == None:
+        print("ids:", ids[0])
+        ids_code=ids[0]
+    print(ids_code)
+    
+    if ids_code != None and ids_code == 2:
+        print(ids_code)
+        yield session.call("rie.dialogue.say", text="Red is certainly a beautiful colour!")
 
 
 @inlineCallbacks
 def behavior(session):
-    yield session.subscribe(aruco, "rom.sensor.sight.stream")
+    yield session.subscribe(aruco_wrapper, "rom.sensor.sight.stream")
     yield session.call("rom.sensor.sight.stream")
-    pass
-
+    
+    def aruco_wrapper(frame):
+        return aruco(session, frame)
+    
+    
 
 def main(session, details):
+    global ids_code
     behavior(session)
-    pass
+        
 
 
 wamp = Component(
@@ -30,7 +43,7 @@ wamp = Component(
             "max_retries": 0,
         }
     ],
-    realm="rie.66cdbc1aafe50d23b76c2e6b",
+    realm="rie.66d1c84cafe50d23b76c5023",
 )
 
 wamp.on_join(main)
